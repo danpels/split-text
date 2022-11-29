dbd22cc2eadb957d287255a534443752b229155c
danpels <100728359+danpels@users.noreply.github.com>
2022-11-28 17:29:39 -0800

    first push
    gsap addition

--- src/main.js
+++ src/main.js
@@ -1,6 +1,31 @@
 import './styles/style.css'
-import feature from './features/features'
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { SplitText } from "gsap/SplitText";
 
-console.log('testing')
+/*
+SplitText.min.js is a Club GreenSock perk
 
-feature()
+import { SplitText } from "gsap/SplitText";
+
+Sign up at https://greensock.com/club or try them for free on CodePen or CodeSandbox
+*/
+
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+let text = new SplitText('.content__text',{
+  type: 'words'
+})
+
+gsap.from(text.words){
+  opacity: 0
+  scrollTrigger:{
+    trigger: '.content__text',
+    start: 'bottom bottom',
+    end: 'top top',
+    scrub: true,
+    pin: '.content__wrapper'
+
+  }
+}
